6e4a4b84e9f0be2ec5b04a4d07878e2ab084ab61
marcus.reiser <marcus.reiser@F406>
2025-12-08 09:35:46 +0100

    Projektdateien hinzufügen.

---
+++ Models/PlacesModels.cs
@@ -0,0 +1,9 @@
+﻿namespace C_Grundlagen.Models
+{
+    public class PlacesModels
+    {
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Views/Shared/_PlaceCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C_Grundlagen.Views.Shared
+{
+    public class _PlaceCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
